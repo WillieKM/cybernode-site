60a17050c9d17a7ba8fdcb440572f4bf29b18303
WillieKM <willie@cyber-node.com>
2026-03-07 17:32:06 -0800

    Update network.js

--- js/network.js
+++ js/network.js
@@ -4,33 +4,38 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let nodes=[];
+let particles=[];
 
-for(let i=0;i<60;i++){
-nodes.push({
+for(let i=0;i<70;i++){
+particles.push({
 x:Math.random()*canvas.width,
 y:Math.random()*canvas.height,
-vx:(Math.random()-0.5)*1,
-vy:(Math.random()-0.5)*1
+vx:(Math.random()-0.5)*0.6,
+vy:(Math.random()-0.5)*0.6
 });
 }
 
-function draw(){
+function animate(){
 
 ctx.clearRect(0,0,canvas.width,canvas.height);
 
-nodes.forEach(n=>{
-n.x+=n.vx;
-n.y+=n.vy;
+particles.forEach(p=>{
+
+p.x+=p.vx;
+p.y+=p.vy;
+
+if(p.x<0||p.x>canvas.width)p.vx*=-1;
+if(p.y<0||p.y>canvas.height)p.vy*=-1;
 
 ctx.beginPath();
-ctx.arc(n.x,n.y,2,0,Math.PI*2);
+ctx.arc(p.x,p.y,2,0,Math.PI*2);
 ctx.fillStyle="#3b82f6";
 ctx.fill();
+
 });
 
-requestAnimationFrame(draw);
+requestAnimationFrame(animate);
 
 }
 
-draw();
+animate();
