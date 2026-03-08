d8bd122bc264a2f18ce885d975b332df764b3ced
WillieKM <willie@cyber-node.com>
2026-03-07 16:30:04 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 
 console.log("Cyber-Node site loaded");
+window.addEventListener("scroll", function(){
+
+let cards = document.querySelectorAll(".solution-card");
+
+cards.forEach(card => {
+
+card.style.opacity = "1";
+
+});
+
+});
